340a94666c8107af5a7c1423f13083e2a76cf242
Николай Ощепков <nikolay.oschepkov@aplana.com>
2018-04-12 15:14:13 +0500

    1

---
+++ yandextests/src/test/java/selenium/selenium/YandexURLSearch.java
@@ -0,0 +1,48 @@
+package selenium.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+
+public class YandexURLSearch {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver;
+        driver = new FirefoxDriver();
+        driver.get("https://yandex.ru/");
+        String serachTerm = "Selenium Webdriver Tutorial - Step by Step Lessons";
+        String matchInUrl = "Selenium Webdriver Tutorial - Step by Step Lessons";
+
+
+        driver.findElement(By.id("text")).sendKeys("selenium webdriver");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        List<WebElement> resultsEntries = (List<WebElement>) driver.findElements(By.className("organic__url-text"));
+        System.out.println(resultsEntries);
+//      Страница результатов поиска
+            int pageNumber=0;
+            boolean foundMatch=false;
+            String foundMatchingUrl="";
+/*            while (!foundMatch){
+
+                pageNumber++;
+                String nextpagebuttonlinktext = "дальше";
+
+            }*/
+            //        Цикл поиска на странице
+            for (WebElement element : resultsEntries) {
+            String href = element.getAttribute("href");
+            String title = element.getText();
+
+            System.out.println(href);
+            System.out.println(title);
+
+            if (href.contains(matchInUrl)) {
+                System.out.println("Ссылка найдена на странице: " + pageNumber);
+                foundMatchingUrl = href;
+                foundMatch = true;
+            } else {
+                driver.findElement(By.linkText("дальше")).click();
+            }
+        }}}
---
+++ yandextests/src/test/java/selenium/selenium/yatestPageSource.java
@@ -0,0 +1,41 @@
+package selenium.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static java.lang.Thread.sleep;
+
+public class yatestPageSource {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver;
+        driver = new FirefoxDriver();
+        driver.get("https://yandex.ru/");
+        driver.findElement(By.id("text")).sendKeys("selenium webdriver");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+/*        List<WebElement> elements = driver.findElements(By.className("link link_theme_normal organic__url link_cropped_no i-bem link_js_inited"));
+        System.out.println(elements);*/
+       // ("Selenium Webdriver Tutorial - Step by Step Lessons")
+        //int pageNumber=1;
+        /*link link_outer_yes link_theme_outer path__item i-bem link_js_inited*/
+        //WebElement page = driver.findElement(By.className("pager__item pager__item_current_yes pager__item_kind_page)"));
+        for(char pageNumber='1';;pageNumber++){
+        {
+            if (driver.getPageSource().contains("toolsqa.com")) {
+                driver.findElement(By.partialLinkText("WebDriver Existence in package org.openqa.selenium")).click();
+                //System.out.println("Ссылка найдена на странице: " + page.getText());
+                System.out.println("Ссылка найдена на странице: " + pageNumber);
+                //sleep(1000);
+                break;
+
+            } else {
+                driver.findElement(By.linkText("дальше")).click();
+                sleep(1000);
+            }
+        }
+
+    }
+}}
+
+//class="link link_theme_normal organic__url link_cropped_no i-bem link_js_inited"
+//class=favicon favicon_page_3
